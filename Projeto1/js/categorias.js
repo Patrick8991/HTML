5e87962722bbd150bc04b044a0b9602a5fadfa60
Yiaannn <Yiaannn@gmail.com>
2016-09-15 22:55:38 +0000

    Código adaptado para ser usado nas outras páginas

--- Projeto1/js/categorias.js
+++ Projeto1/js/categorias.js
@@ -9,21 +9,6 @@ ctx.font="14px calibri"; //TODO: usar uma fonte embasada
 document.onmousemove= updateMousePos;
 document.onclick= mouseSelect;
 
-var buttons= ["Projetos Teóricos", "Projetos Práticos", "Anáise Matemática", "Direcionamento e Execução de Projetos",  "Creative Thinking", "Desenvolvimento e Aplicação de Software", "Circuitos Elétricos"];
-var column_0= ["Projetos Práticos", "Direcionamento e Execução de Projetos", "Desenvolvimento e Aplicação de Software"];
-var column_1= ["Projetos Teóricos", "Análise Matemática", "Creative Thinking", "Circuitos Elétricos"];
-var center_0= [];
-var center_1= [];
-var miscCounter_0= [0, 0, 0];
-var miscCounter_1= [0, 0, 0, 0];
-var rad= ctx.canvas.height/6;
-var spc_0= (ctx.canvas.width- 6*rad)/8
-var spc_1= (ctx.canvas.width- 8*rad)/8
-
-
-var mouse_x= 0;
-var mouse_y= 0;
-
 //buttons
 class Button{
     constructor(position, label, redirect) {
@@ -38,25 +23,15 @@ class Button{
         this.counter= 0
         this.colors=[
             "#9c1f18",
-            "rgb(160, 36, 24)",
             "rgb(165, 42, 23)",
-            "rgb(170, 47, 23)",
             "rgb(175, 53, 22)",
-            "rgb(180, 58, 22)",
             "rgb(185, 64, 21)",
-            "rgb(190, 69, 21)",
             "rgb(195, 75, 20)",
-            "rgb(200, 80, 20)",
             "rgb(205, 86, 19)",
-            "rgb(210, 91, 19)",
             "rgb(215, 97, 18)",
-            "rgb(220, 102, 18)",
             "rgb(225, 108, 17)",
-            "rgb(230, 113, 17)",
             "rgb(235, 119, 16)",
-            "rgb(240, 124, 16)",
             "rgb(245, 130, 15)",
-            "rgb(250, 135, 15)",
             "#FE8D0E"
         ]
     }
@@ -64,9 +39,9 @@ class Button{
     update(){
         //logic
         this.counter+=(this.mouseover ? 1 : -1);
-        if (this.counter<0 || this.counter>20){
+        if (this.counter<0 || this.counter>10){
             this.counter+=1;
-            this.counter-=(this.counter==22? 2 : 0);
+            this.counter-=(this.counter==12? 2 : 0);
         }else{
             this.redraw= true;
         }
@@ -98,36 +73,44 @@ class Button{
     }
 }
 
-for (var i = 0; i < column_0.length; ++i) {
-    
-    center_0.push( [(2*spc_0+rad)+(i*(2*rad)+i*(2*spc_0)), 1.5*rad] )
-    console.log(center_0[-1]);
-}
-
-for (var i = 0; i < column_1.length; ++i) {
-    
-    center_1.push( [(1*spc_1+rad)+(i*(2*rad)+i*(2*spc_1)), 4.5*rad] )
-}
-
 var buttons= [];
-buttons.push( new Button( center_1[0], "Projetos Teóricos", "ProjetosTeóricos.html"))
-buttons.push( new Button( center_0[0], "Projetos Práticos", "ProjetosPráticos.html"))
-buttons.push( new Button( center_1[1], "Anáise Matemática", "AnáliseMatemática.html"))
-buttons.push( new Button( center_0[1], "Direcionamento e Execução de Projetos", "DirecionamentoExecução.html"))
-buttons.push( new Button(center_1[2], "Creative Thinking", "CreativeThinking.html"))
-buttons.push( new Button( center_0[2], "Desenvolvimento e Aplicação de Software", "DesenvolvimentoAplicação.html"))
-buttons.push( new Button( center_1[3], "Circuitos Elétricos", "CircuitosElétricos.html"))
-
-//depois acerto uma função melhor para calcular o centro dos botões
-
+var rad;
+function buildButtons(){
+    var labels=[
+        "Projetos Teóricos",
+        "Projetos Práticos",
+        "Anáise Matemática",
+        "Direcionamento e Execução de Projetos",
+        "Creative Thinking",
+        "Desenvolvimento e Aplicação de Software",
+        "Circuitos Elétricos"
+    ]
+    
+    var redirects=[
+        "ProjetosTeóricos.html",
+        "ProjetosPráticos.html",
+        "AnáliseMatemática.html",
+        "DirecionamentoExecução.html",
+        "CreativeThinking.html",
+        "DesenvolvimentoAplicação.html",
+        "CircuitosElétricos.html"
+    ]
+    
+    rad= ctx.canvas.width/(2*(labels.length+1));
+    
+    for (var i = 0; i < labels.length; ++i) {
+        var position= [2*rad+2*i*rad, (ctx.canvas.height/4)*(1+2*(i%2))];
+        buttons.push(new Button( position, labels[i], redirects[i]));
+    }
+    
+    //depois acerto uma função melhor para calcular a posição dos botões
+}
 
 //mouse events
-
 var mouseover= null;
-
 function updateMousePos(event){
-    mouse_y= event.clientY - canvas.offsetTop;
-    mouse_x= event.clientX - canvas.offsetLeft;
+    var mouse_y= event.clientY - canvas.offsetTop;
+    var mouse_x= event.clientX - canvas.offsetLeft;
     
     mouseover= null;
     for (var i = 0; i < buttons.length; ++i) {
@@ -151,6 +134,7 @@ function mouseSelect(event){
 }
 
 //main
+buildButtons();
 
 function main(){
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -163,4 +147,4 @@ function main(){
         btn.update();
     });
 }
-window.setInterval(main, 20);
+window.setInterval(main, 40);
